016be9c897dd720b1ffd8f0a3e79a55512c30d1a
Stepan Skichko <st.skichko@gmail.com>
2023-02-12 13:17:12 +0200

    added timeout task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+const bodyTag = document.querySelector('body');
+
+// btnStart.dataset;
+
+let timerId = null;
+
+btnStart.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    bodyTag.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+btnStop.addEventListener('click', () => {
+  clearInterval(timerId);
+});
+
+// function onBtnStartClick() {
+//   const timerId = setInterval(() => {
+//     bodyTag.style.backgroundColor = getRandomHexColor();
+//   }, 1000);
+// }
+
+// function onBtnStopClick() {
+//   clearInterval(timerId);
+// }
